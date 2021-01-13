e2682ad5c3b589cd7eb0f546acb7549bbb4e8b07
HeeYeonKim98 <wow9896@naver.com>
2021-01-13 20:37:47 +0900

    210113

--- python/codeup/1061-1070.py
+++ python/codeup/1061-1070.py
@@ -1,1 +1,33 @@
 # 1061 - 1070
+
+# 1061 : 비트 단위로 or하여 출력
+a,b=input().split(" ")
+a=int(a)
+b=int(b)
+print(a | b)
+
+# 1062 : 비트 단위로 xor하여 출력
+a,b=input().split(" ")
+a=int(a)
+b=int(b)
+print(a ^ b)
+
+# 1063 : 정수 2개 입력받아 큰 수 출력
+a,b=input().split(" ")
+a=int(a)
+b=int(b)
+print(a if a>b else b)
+
+# 1064 : 정수 3개 입력받아 작은 수 출력
+a,b,c=input().split(" ")
+a=int(a)
+b=int(b)
+c=int(c)
+print((a if a<b else b) if (a if a<b else b)<c else c)
+
+# 1065 : 
+# 1066 : 
+# 1067 : 
+# 1068 : 
+# 1069 : 
+# 1070 : 
--- python/codeup/debug.py
+++ python/codeup/debug.py
@@ -1,21 +1,5 @@
-#a:고정비용 b:가변비용 c:노트북 가격 income : 총 수입 cost : 총 비용
-
-# a,b,c=input().split(" ")
-# a=int(a)
-# b=int(b)
-# c=int(c)
-# for n in range(1,1000):
-#     income=c*n
-#     cost=a+b*n
-
-#     if income>cost:
-#         print(n)
-#         break
-
-# print()
-
-a,b=input().split(" ")
+a,b,c=input().split(" ")
 a=int(a)
 b=int(b)
-print(a & b)
-
+c=int(c)
+print((a if a<b else b) if (a if a<b else b)<c else c)
