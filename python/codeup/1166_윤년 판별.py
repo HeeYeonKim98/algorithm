bcf9dae3bceb80c8af688f04e575cff4ba6c0a4c
HeeYeonKim98 <wow9896@naver.com>
2021-02-16 23:06:23 +0900

    210216

--- python/codeup/1166_윤년 판별.py
+++ python/codeup/1166_윤년 판별.py
@@ -1,3 +1,16 @@
 # 1166
 
 # 해를 입력받아 그 해가 윤년인지 아닌지 판별하시오.
+
+year=int(input())
+
+if year%4==0:
+    if year%100!=0:
+        print("yes")
+    elif year%400==0:
+        print("yes")
+    else:
+        print("no")
+
+else:
+    print("no")
