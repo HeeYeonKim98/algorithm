efc4545ee1d7097df7d901e26a6c1245fcc741be
HeeYeonKim98 <wow9896@naver.com>
2021-02-23 19:28:23 +0900

    210223

---
+++ python/programmers/Level 1/가운데 글자 가져오기.py
@@ -0,0 +1,19 @@
+# 단어의 가운데 글자를 반환하는 함수, solution을 만드시오.
+
+# 나의 풀이
+
+def solution(word):
+    leng=len(word)
+    if leng%2==0:
+        answer=word[leng//2-1:leng//2+1]
+        return answer
+    else:
+        answer=word[leng//2:leng//2+1]
+        return answer
+
+# 좋은 풀이
+
+def string_middle(str):
+    return str[(len(str)-1)//2:len(str)//2+1]
+
+print(string_middle("power"))
