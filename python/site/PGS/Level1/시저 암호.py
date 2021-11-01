e43121b3a7de13f5133bc41090de891bd2b43bc8
HeeYeonKim98 <wow9896@naver.com>
2021-11-01 23:48:22 +0900

    211101 renewal-1

---
+++ python/site/PGS/Level1/시저 암호.py
@@ -0,0 +1,8 @@
+# 시저암호 만들기
+
+# 풀이
+
+def solution(s,n):
+    return 0
+
+print(solution("AB",1))
---
+++ python/site/PGS/Level1/체육복.py
@@ -0,0 +1,23 @@
+# 전체 학생 수(n), 체육복을 도난당한 학생 번호 배열(lost),
+# 여벌의 체육복을 가져온 학생 번호 배열(reserve)가 있을 때, 체육수업을 들을 수 있는 학생의 최댓값을 반환
+
+# 풀이
+
+# def solution(n,lost,reserve):
+#     return 0
+
+# n=5
+# lost=[2,4]
+# reserve=[1,3,5]
+# print(solution(n,lost,reserve))
+
+i=0
+sum=0
+while i<10:
+    i=i+1
+    if i%2==0:
+        continue
+    sum=sum+i
+
+
+print(sum)
